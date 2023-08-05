8cab5c1e588c7c11c07938b3b87798749b214f14
Mikebulatov <mikebulatov@yandex.ru>
2023-08-05 10:48:18 +0700

    Задача 6. Изменение 1

--- zd8/Program.cs
+++ zd8/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Задача 6: Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
-int num;
+Console.WriteLine("Задача 8: Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
+int i,N;
 Console.Write("Введите  число:");
 num = Convert.ToInt32(Console.ReadLine());
 
