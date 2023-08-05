d6818b1b48db00cad1c88d7b062e2071f5455ad8
Mikebulatov <mikebulatov@yandex.ru>
2023-08-05 10:57:47 +0700

    Задача 8

--- zd8/Program.cs
+++ zd8/Program.cs
@@ -2,16 +2,12 @@
 Console.WriteLine("Задача 8: Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
 int i,N;
 Console.Write("Введите  число:");
-num = Convert.ToInt32(Console.ReadLine());
-
-if(num%2>0)
-
-{
-    Console.WriteLine("Число не четное");
-}
-else
-{
-
-    Console.WriteLine("Число четное");
-
+N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа:");
+for(i=1;i<=N;i++){
+if(i%2==0)
+    {
+    Console.Write(i);
+    Console.Write(", ");
+    }
 }
