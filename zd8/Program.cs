57be9e77f501d65a81eaaa0421acf3f9fc88f517
Mikebulatov <mikebulatov@yandex.ru>
2023-08-06 01:04:06 +0700

    Задача 8. Изменение 1.

--- zd8/Program.cs
+++ zd8/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Задача 8: Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
-int i,N;
+//int i,N;
 Console.Write("Введите  число:");
-N = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Четные числа:");
-for(i=1;i<=N;i++){
+for(int i=1;i<=N;i++){
 if(i%2==0)
     {
     Console.Write(i);
