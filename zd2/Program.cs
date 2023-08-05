287fde34d4d48ab428c5a9921fe64acc97d6bd19
Mikebulatov <mikebulatov@yandex.ru>
2023-08-05 10:10:17 +0700

    Задача 2. Изменение 1

--- zd2/Program.cs
+++ zd2/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Задача 2: Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.Hello, World!");
+Console.WriteLine("Задача 2: Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее");
 int num1,num2;
+Console.Write("Введите 1е число:");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2е число:");
+num2 = Convert.ToInt32(Console.ReadLine());
