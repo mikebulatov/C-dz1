7f8dbc3a531bf2eff6657da05340ded8d0f75360
Mikebulatov <mikebulatov@yandex.ru>
2023-08-05 09:31:33 +0700

    Задача2

---
+++ zd2/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задача 2: Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.Hello, World!");
+int num1,num2;
